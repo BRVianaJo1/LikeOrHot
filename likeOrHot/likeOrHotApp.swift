a2c8fd509e79dc963bb24efcedb0244d84c7627a
Joao Bonilha <joao.viana@shopper.com.br>
2023-11-28 20:31:02 -0300

    Initial Commit

---
+++ likeOrHot/likeOrHotApp.swift
@@ -0,0 +1,17 @@
+//
+//  likeOrHotApp.swift
+//  likeOrHot
+//
+//  Created by Joao on 28/11/23.
+//
+
+import SwiftUI
+
+@main
+struct likeOrHotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
